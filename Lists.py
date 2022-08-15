8730555a0e41599983613dc58fce2ca4a273294a
AlexGorSvami <jagger15696@gmail.com>
2022-08-15 11:26:10 +0300

    First

---
+++ Lists.py
@@ -0,0 +1,18 @@
+# def first_word(str):
+#     for i in str:
+#         print(i, end='')
+#         if i == ' ':
+#             break
+#
+#
+# assert first_word("Hello world") == "Hello"
+# assert first_word("a word") == "a"
+# assert first_word("greeting from CheckiO Planet") == "greeting"
+# assert first_word("hi") == "hi"
+#
+#
+#
+# def first_word(text: str) -> str:
+#     return text.replace(".", " ").replace(",", " ").split()[0]
+
+
