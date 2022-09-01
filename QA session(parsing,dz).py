7879f1de601f1c510c8d39a3111b09ac23e97d27
AlexGorSvami <jagger15696@gmail.com>
2022-09-01 18:28:12 +0300

    Mos-newFirst

---
+++ QA session(parsing,dz).py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+res = requests.get('https://netology.ru/blog/')
+
+
+
+
+# soup = BeautifulSoup(res.text)
+# print(soup)
+
+
+from pprint import pprint
+
+
+# posts = soup.find_all('div', class_='posts__item')
+# pprint(posts)
+#
+# for post in posts:
+#     title = post.find('a', class_='posts__link').text
+#     print(title)
+#     date = post.find('div', class_='posts__date').text
+#     print(date)
+#     link = post.find('a', class_='posts__link').get('href')
+#     print(link)
+#     category = post.find('a', class_='tags__item').text
+#     print(category)
+
+
+URL = 'https://netology.ru/blog/?s=python'
+
+res = requests.get(URL)
+
+soup = BeautifulSoup(res.text)
+
+news = soup.find_all('article', class_='status-publish')
+print(news)
+
+for post in news:
+    header = post.find('h2', 'entry-title')
+    print(header)
+    link = post.find('a').get('h1')
+    print(link)
+
