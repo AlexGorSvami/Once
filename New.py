253c7675739709911a6becb646656bb9331951f5
AlexGorSvami <jagger15696@gmail.com>
2022-09-05 08:41:44 +0300

    Os-new1

--- New.py
+++ New.py
@@ -1,3 +1,20 @@
-n = int(input())
-h = (n // 60) % 24
-print(h, n % 60)
+# n = int(input())
+# h = (n // 60) % 24
+# print(h, n % 60)
+#
+# num = int(input())
+# end ='.'
+# print('The next number for the number', str(num), 'is', str(num+1)+'.')
+# print ('The previous number for the number', str(num), 'is', str(num-1)+'.')
+
+# a,b,c = int(input()),int(input()),int(input())
+# print((a // 2 + a % 2) + (b // 2 + b % 2) + (c // 2 + c % 2))
+#
+# a,b,l,N = int(input()),int(input()),int(input()),int(input())
+# print(2*l+(2*N-1)*a+2*(N-1)*b)
+
+# a,b,c,d = int(input()),int(input()),int(input()),int(input())
+# if (a+b) % 2 == 0 and (c+d) % 2 == 0 or (a+b) % 2 != 0 and (c+d) % 2 != 0:
+#     print('YES')
+# else:
+#     print('NO')
