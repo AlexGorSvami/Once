ed3c204281e8c28c2d67223b397de45fd0f36604
AlexGorSvami <jagger15696@gmail.com>
2022-09-08 06:16:11 +0300

    Os-new4

--- New.py
+++ New.py
@@ -65,13 +65,68 @@
 # print('Example:')
 # print(between_markers('What is >apple<', '>', '<'))
 
-def replace_first(items: list):
-    if len(items) == 0 or len(items) == 1:
-        return items
-    else:
+# def replace_first(items: list):
+#     new = 1
+#     if len(items) == 0 or len(items) == 1 or len(items) == 2:
+#         return items
+#     else:
+#         items[0] = new
+#         items.pop(0)
+#         items.append(new)
+#     return items
+#
+# def replace_first(lst):
+#     if len(lst):
+#         lst.append(lst.pop(0))
+#     return lst
+#
+# replace_first = lambda l: l[1%len(l):]+l[:1%len(l)] if not len(l)==0 else l
+#
+#
+# from typing import Iterable
+# import numpy as np
+#
+# def replace_first(items: list) -> Iterable:
+#     # your code here
+#     return np.roll(items,-1)
+#
+#
+# if __name__ == '__main__':
+#     print("Example:")
+#     print(list(replace_first([1, 2, 3, 4])))
+
+# def correct_sentence(text: str) -> str:
+#     return text.replace(text[0], text[0].upper(), 1)
+#
+# print('Example:')
+# print(correct_sentence("greetings, friends"))
+
+# def best_stock(data: dict[str, float]) -> str:
+#     for i, j in data.items():
+#         if j == max(data.values()):
+#             return i
+#
+#
+# print("Example:")
+# print(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}))
 
 
+# def nearest_value(values: set, one: int) -> int:
+#     if one in values:
+#         return one
+#     else:
+#         for i in values:
+#             if
+#
+#
+#
+# if __name__ == '__main__':
+#     print("Example:")
+#     print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
 
+
+a, b = map(int, input().split())
+print((a+b-2)-(a-1), (a+b-2)-(b-1))
 
 
 
