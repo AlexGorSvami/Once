5f5a8b673bed4c03485187f2eb8528cab59d4613
AlexGorSvami <jagger15696@gmail.com>
2022-11-28 13:08:40 +0500

    UZ-23

--- Python для pre-junior Backend-разработчика.py
+++ Python для pre-junior Backend-разработчика.py
@@ -29,3 +29,46 @@
 # print(13 << 2 >> 1)
 
 # print(int(input()) ^ 47) XOR шифрование
+
+
+# n1, n2, n3, n4 = [int(i) for i in input().split('.')]
+# if n1 == 0 and n2 == 0 and n3 == 0 and n4 == 0:
+#     print(False)
+# elif n1 == 255 and n2 == 255 and n3 == 255 and n4 == 255:
+#     print(False)
+# elif n1 > 255 or n2 > 255 or n3 > 255 or n4 > 255:
+#     print(False)
+# else:
+#     print(True)
+#
+#
+# n1, n2, n3, n4 = [int(i) for i in input().split('.')]
+# if n1 == n2 == n3 == n4 == 0 or n1 == n2 == n3 == n4 == 255:
+#     print(False)
+# elif 0 <= n1 <= 255 and 0 <= n2 <= 255 and 0 <= n3 <= 255 and 0 <= n4 <= 255:
+#     print(True)
+# else:
+#     print(False)
+
+
+# n = int(input())
+# days = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
+# print(days[n % 7])
+
+
+# n = int(input()) % 7
+# if n == 0:
+#     print('пн')
+# elif n == 1:
+#     print('вт')
+# elif n == 2:
+#     print('ср')
+# elif n == 3:
+#     print('чт')
+# elif n == 4:
+#     print('пт')
+# elif n == 5:
+#     print('сб')
+# else:
+#     print('вс')
+
