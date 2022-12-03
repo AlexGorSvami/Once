8d868330f702c0519640e9f5e553e5d52782a392
AlexGorSvami <jagger15696@gmail.com>
2022-12-03 11:54:45 +0500

    UZ-29

--- 50 задач Python.py
+++ 50 задач Python.py
@@ -18,11 +18,16 @@
 #     print(i)
 
 
-a = list(map(int, input().split()))
-counter = {}
-for elem in a:
-    counter[elem] = counter.get(elem, 0) + 1
+# a = list(map(int, input().split()))
+# counter = {}
+# for elem in a:
+#     counter[elem] = counter.get(elem, 0) + 1
+#
+# doubles = {element: count for element, count in counter.items if count > 2}
+# for i in sorted(doubles.keys()):
+#     print(i)
 
-doubles = {element: count for element, count in counter.items if count > 2}
-for i in sorted(doubles.keys()):
-    print(i)
+a = [1, 3, 4, 5, 3, 3, 44, 1, 14, 15, 4, 1, 44]
+new = []
+[new.append(i) for i in a if i not in new]
+print(*new)
