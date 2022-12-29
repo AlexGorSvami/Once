4312d68439dc7477c8d7ce08d01b112931918d22
AlexGorSvami <jagger15696@gmail.com>
2022-12-29 07:03:56 +0500

    UZ-55

--- Python для pre-junior Backend-разработчика.py
+++ Python для pre-junior Backend-разработчика.py
@@ -261,19 +261,32 @@
 #     print(a[l])
 
 
-commands = ['git push origin main']
+# commands = ['git push origin main']
+#
+# for command in commands:
+#     match command.split():
+#         case 'git', 'add', path:
+#             print(f'В индекс добавлены файлы по пути: {path}.')
+#         case 'git', 'commit':
+#             print('Файлы закоммичены.')
+#         case 'git', 'commit', '-m', message:
+#             print(f'Файлы закоммичены с сообщением {message}')
+#         case 'git', 'push', repository, branch:
+#             print(f'Файлы запушены в {repository} в ветку {branch}.')
+#         case _:
+#             print('Неизвестная команда.')
 
-for command in commands:
-    match command.split():
-        case 'git', 'add', path:
-            print(f'В индекс добавлены файлы по пути: {path}.')
-        case 'git', 'commit':
-            print('Файлы закоммичены.')
-        case 'git', 'commit', '-m', message:
-            print(f'Файлы закоммичены с сообщением {message}')
-        case 'git', 'push', repository, branch:
-            print(f'Файлы запушены в {repository} в ветку {branch}.')
-        case _:
-            print('Неизвестная команда.')
 
-print(69,53/13,3)
+# a = set()
+# while i := int(input()):
+#     a.add(i)
+# print(len(a)+1)
+
+print(
+    len(
+        {
+            i.replace('b','d',1) if i.startswith('b') else i.replace('c','')
+            for i in ['aa','bbb','cccc', 'bacbac'] if len(i) > 2
+        }
+    )
+)
