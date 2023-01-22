4d35bb59808c1cb1c23b9bfef322b643a2ca0973
AlexGorSvami <jagger15696@gmail.com>
2023-01-22 15:21:04 +0500

    uz23-20

--- Python: основы и применение.py
+++ Python: основы и применение.py
@@ -4,3 +4,13 @@
 #     count += int(input())
 # print(count)
 
+def s(a, *vs, b=10):
+    res = a + b
+    for v in vs:
+        res +=  v
+    return res
+
+print(s(11,b=20))
+print(s(11,10))
+print(s(21))
+print(s())
