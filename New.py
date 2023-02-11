b580593495f1e4a2d89d978d1fce6a657b61cf47
AlexGorSvami <jagger15696@gmail.com>
2023-02-11 11:09:21 +0500

    uz2-11

--- New.py
+++ New.py
@@ -371,5 +371,69 @@
 #     print(n // 64,end='  ')
 
 
-mas = [int(i) for i in input().split()]
-print(max(mas),min(mas),(mas[len(mas)//2] + mas[(len(mas)-1)//2])/2)
+# mas = [int(i) for i in input().split()]
+# print(max(mas),min(mas),(mas[len(mas)//2] + mas[(len(mas)-1)//2])/2)
+
+# from math import sqrt
+#
+#
+# class Point:
+#
+#     list_points = []
+#     def __init__(self, x=0, y=0):
+#         self.move_to(x, y)
+#         Point.list_points.append(self)
+#
+#     def move_to(self, new_x, new_y):
+#         self.x = new_x
+#         self.y = new_y
+#
+#     def go_home(self):
+#         self.move_to(0, 0)
+#
+#     def print_point(self):
+#         print(self.x, self.y)
+#
+#     def calc_distance(self, another_point):
+#         if not isinstance(another_point, Point):
+#             raise ValueError('Argument not touch')
+#
+#         return sqrt((self.x - another_point.x) ** 2 + (self.y - another_point.y) ** 2)
+#
+#
+# p7 = Point(6, 0)
+# p8 = Point(0, 8)
+# p11 = Point(1,12)
+#
+# print(Point.list_points[1].x)
+
+
+class Person:
+    def __init__(self,name,age):
+        self.name= name
+        self.age = age
+
+    def display_person_info(self):
+        print(f'Person: {self.name}, {self.age}')
+
+class Company:
+    def __init__(self,company_name, location):
+        self.company_name = company_name
+        self.location = location
+
+    def display_company_info(self):
+        print(f'Company: {self.company_name}, {self.location}')
+
+class  Employee:
+    def __init__(self,name,  age, company_name, location):
+        self.personal_data  = Person(name,age)
+        self.work =  Company(company_name, location)
+
+
+emp = Employee('Jessica', 28, 'Google', 'Atlanta')
+print(emp.personal_data.name)
+print(emp.personal_data.age)
+emp.personal_data.display_person_info()
+print(emp.work.company_name)
+print(emp.work.location)
+emp.work.display_company_info()
