52db1ab9b1d9c4e75409180a21ab47c4441cc2c5
AlexGorSvami <jagger15696@gmail.com>
2023-03-13 23:01:04 +0500

    uz3-10

--- New.py
+++ New.py
@@ -1024,18 +1024,66 @@ import sys
 #         fib_rec(N, f)
 #     return f
 
+#
+# d = [1, 2, [True, False], ["Москва", "Уфа", [100, 101], ['True', [-2, -1]]], 7.89]
+#
+# def get_line_list(d,a=[]):
+#     # if a == None:
+#     #     a = []
+#     for i in d:
+#         if type(i) != list:
+#             a.append(i)
+#         else:
+#             get_line_list(i)
+#     return a
+#
+# print(get_line_list(d))
 
-d = [1, 2, [True, False], ["Москва", "Уфа", [100, 101], ['True', [-2, -1]]], 7.89]
 
-def get_line_list(d,a=[]):
-    # if a == None:
-    #     a = []
-    for i in d:
-        if type(i) != list:
-            a.append(i)
-        else:
-            get_line_list(i)
-    return a
+class Employee:
+    def __init__(self, name, salary):
+        self.__name = name
+        self.__salary = salary
 
-print(get_line_list(d))
+    def __get_name(self):
+        return self.__name
 
+    def __get_salary(self):
+        return self.__salary
+
+    def __set_salary(self, value):
+        if (type(value) ==  float or type(value) == int) and value > 0:
+            self.__salary = value
+        print(f'ErrorValue:{value}')
+
+    title = property(
+        fget=__get_name)
+
+    reward = property(
+        fget=__get_salary,
+        fset=__set_salary
+    )
+
+
+# Ниже код для проверки методов класса Employee
+employee = Employee("John Doe", 50000)
+assert employee.title == "John Doe"
+assert employee._Employee__name == "John Doe"
+assert isinstance(employee, Employee)
+assert isinstance(type(employee).title, property), 'Вы не создали property title'
+assert isinstance(type(employee).reward, property), 'Вы не создали property reward'
+
+assert employee.reward == 50000
+employee.reward = -100  # ErrorValue:-100
+
+employee.reward = 1.5
+assert employee.reward == 1.5
+
+employee.reward = 70000
+assert employee.reward == 70000
+employee.reward = 'hello'  # Печатает ErrorValue:hello
+employee.reward = '777'  # Печатает ErrorValue:777
+employee.reward = [1, 2]  # Печатает ErrorValue:[1, 2]
+assert employee.reward == 70000
+employee._Employee__set_salary(55000)
+assert employee._Employee__get_salary() == 55000
